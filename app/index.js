5330887f896f3ac2beaebfea909791fb8e88a75d
adafia <adafia.samuel@gmail.com>
2020-01-19 14:37:17 +0200

    create data storage system and store

--- app/index.js
+++ app/index.js
@@ -10,6 +10,14 @@ const url = require('url');
 const StringDecoder = require('string_decoder').StringDecoder;
 const config = require('./config');
 const fs = require('fs');
+const _data = require('./lib/data');
+
+
+// Testing
+// @Todo delete this
+_data.delete('test', 'newfile', (err) => {
+  console.log('this was the error', err);
+});
 
 // Instantiate the HTTP server
 const httpServer = http.createServer(function(req, res) {
